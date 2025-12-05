15ae06d8eaeabd0765c3b896b14c142c22fd18aa
tamoenpeso <gabriel.catral@educa.madrid.org>
2025-12-05 13:55:08 +0100

    comentario

---
+++ 2/prueba1.js
@@ -0,0 +1,13 @@
+
+var mensaje = "hola mundo que tal";
+console.log(mensaje);
+console.log(mensaje);
+console.log(document);
+console.log(localStorage);
+
+alert("prueba de codigo");
+var esViernes = true;
+console.log("Es viernes: " + environment);
+if (esViernes == true){
+    console.log("Por fin es viernes");
+}
